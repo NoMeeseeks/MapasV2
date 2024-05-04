90afc339f1ce1c37ff3ad9f1dd8b42c04d6c3e51
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-04 15:08:22 -0500

    Mostrar el mapa

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
+import { PlacesService } from '../../services';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent {
+export class MapViewComponent implements AfterViewInit {
+  public placesServices = inject(PlacesService)
 
+  @ViewChild('mapDiv') mapDivElement!: ElementRef;
+
+  ngAfterViewInit(): void {
+    if (!this.placesServices.userLocation) throw Error('No hay placesService.userLocation');
+
+    new Map({
+      container: this.mapDivElement.nativeElement,
+      style: 'mapbox://styles/mapbox/light-v12', // style URL
+      center: this.placesServices.userLocation,
+      zoom: 14,
+    });
+  }
 }
