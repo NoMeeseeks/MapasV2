71f286aeca69635544c21d6714aba889dc368527
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-04 15:31:21 -0500

    cambios visuales en el mapa

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -10,16 +10,67 @@ import { Map } from 'mapbox-gl';
 export class MapViewComponent implements AfterViewInit {
   public placesServices = inject(PlacesService)
 
-  @ViewChild('mapDiv') mapDivElement!: ElementRef;
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef
+
 
   ngAfterViewInit(): void {
     if (!this.placesServices.userLocation) throw Error('No hay placesService.userLocation');
 
-    new Map({
-      container: this.mapDivElement.nativeElement,
-      style: 'mapbox://styles/mapbox/light-v12', // style URL
+    const map = new Map({
+      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+      style: 'mapbox://styles/mapbox/standard',
+
       center: this.placesServices.userLocation,
       zoom: 14,
+      container: this.mapDivElement.nativeElement,
     });
+
+    map.on('style.load', () => {
+      // Insert the layer beneath any symbol layer.
+      const layers = map.getStyle().layers;
+
+      // The 'building' layer in the Mapbox Streets
+      // vector tileset contains building height data
+      // from OpenStreetMap.
+      map.addLayer(
+        {
+          'id': 'add-3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 15,
+          'paint': {
+            'fill-extrusion-color': '#aaa',
+
+            // Use an 'interpolate' expression to
+            // add a smooth transition effect to
+            // the buildings as the user zooms in.
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.6
+          }
+        },
+
+      );
+    });
+
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2LXhhdmllciIsImEiOiJjbHV5OXRpOXgwNjUyMmlvYWdoa290NnFwIn0.XetX3TUEducu_MnsP4X3_g'
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2LXhhdmllciIsImEiOiJjbHV5OXRpOXgwNjUyMmlvYWdoa290NnFwIn0.XetX3TUEducu_MnsP4X3_g';
 
 if (!navigator.geolocation) {
   alert('Tu navegador no soporta la Geolocalizacion')
